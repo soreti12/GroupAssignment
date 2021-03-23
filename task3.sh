178f7f826f3498ae6c50f8efbade3c405e3091f0
noor-soreti <noor.said0698@gmail.com>
2021-03-23 14:24:24 -0400

    task3 completed

--- task3.sh
+++ task3.sh
@@ -1,8 +1,23 @@
+# GET INPUT FROM USER
 read -p "Please enter a number: " NUM
 
 echo "You entered $NUM"
 
+OUTPUT=0
+
 for (( i=1; i<=$NUM; i++ ))
 do
-	echo $i
+	for (( j=1; j<=i; j++ ))
+	do
+		if [ `expr $i % $j` -eq 0 ]
+		then
+			if [ `expr $i / $j` -eq `expr $j + 1` ]
+			then
+				echo $i
+				OUTPUT=`expr $OUTPUT + 1`
+			fi
+		fi
+	done
 done
+
+echo "Total output: $OUTPUT"
