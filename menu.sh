f6793f97f38c496307ba073ddb9d43f85fcdf709
mufasa90 <80981799+mufasa90@users.noreply.github.com>
2021-03-24 14:59:51 -0400

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -20,3 +20,24 @@ then
 else 
 	echo "ERROR, please enter a valid input"
 fi
+
+
+
+
+or
+
+while true; do 
+
+echo "Welcome To Group 16 Project"
+echo "Here you will find three tasks"
+echo "Task 1 List of even multiples of any number [Mm]"
+echo "Task 2 List of terms of any linear sequence[Ss]"
+echo "Task 3 List of two non negative numbers in succession [Pp]"
+echo "Quit application [Qq]"
+
+read -n 1 -r -s -p $'Choose a Task: ' Taskselected
+
+case $Taskselected in
+
+[Mm])
+clear
